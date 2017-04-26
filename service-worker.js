51afe89d094a1d3b9c5cc6fb6a30fe2629e0d1cb
prabhjotkaur10 <prabhjot@addvalsolutions.com>
2017-04-26 12:09:03 +0530

    changes path for service worker in app.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-var dataCacheName = 'weatherData-v1';
-var cacheName = 'weatherPWA-step-6-4';
+var dataCacheName = 'weatherData-v3';
+var cacheName = 'weatherPWA-step-6-7';
 var filesToCache = [
   '/',
   'index.html',
